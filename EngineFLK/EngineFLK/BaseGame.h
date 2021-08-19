8c59ec4f308f8dfcf101b71f897e5ce3e64cb528
HavensurgeSW <matias@karplus.com.ar>
2021-08-19 11:05:07 -0300

    Emptied Main function to BaseGame function ->launchGod();

--- EngineFLK/EngineFLK/BaseGame.h
+++ EngineFLK/EngineFLK/BaseGame.h
@@ -1,5 +1,21 @@
 #pragma once
-class BaseGame{
+#include <GLFW\glfw3.h>
+#include "CollisionManager.h"
+#include "Input.h"
+#include "Renderer.h"
+#include "Window.h"
+#include <iostream>
 
+class BaseGame{
+private:
+    Window* w;
+    Renderer* r;
+    Input* i;
+    CollisionManager* cm;
+public:
+    void launchGod();
 };
+
+
+
 
