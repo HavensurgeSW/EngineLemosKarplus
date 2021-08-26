2d499001887e6bbfbd99786655da9b188930e4f4
HavensurgeSW <matias@karplus.com.ar>
2021-08-26 09:22:39 -0300

    Added GLEW lib to WIN, REN, BASE.h's

--- EngineFLK/src/BaseGame.h
+++ EngineFLK/src/BaseGame.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "glew.h"
 #include <GLFW\glfw3.h>
 
 #include "CollisionManager.h"
--- EngineFLK/src/Renderer.h
+++ EngineFLK/src/Renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Window.h"
+#include "glew.h"
 #include "GLFW/glfw3.h"
 
 class Renderer {
