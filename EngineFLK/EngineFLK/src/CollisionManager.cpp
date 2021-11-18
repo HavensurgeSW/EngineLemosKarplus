7911ea2efbf1cb60947420efde772ec6da8d033d
HavensurgeSW <matias@karplus.com.ar>
2021-11-18 09:36:13 -0300

    Drafted first collision model

--- EngineFLK/EngineFLK/src/CollisionManager.cpp
+++ EngineFLK/EngineFLK/src/CollisionManager.cpp
@@ -1,1 +1,13 @@
 #include "CollisionManager.h"
+
+bool CollisionManager::CheckCollision(Entity2D& one, Entity2D& two)
+{
+    // collision x-axis?
+    bool collisionX = one.Position.x + one.Size.x >= two.Position.x &&
+        two.Position.x + two.Size.x >= one.Position.x;
+    // collision y-axis?
+    bool collisionY = one.Position.y + one.Size.y >= two.Position.y &&
+        two.Position.y + two.Size.y >= one.Position.y;
+    // collision only if on both axes
+    return collisionX && collisionY;
+}
--- EngineFLK/EngineFLK/src/CollisionManager.h
+++ EngineFLK/EngineFLK/src/CollisionManager.h
@@ -1,12 +1,14 @@
 #ifndef COLLISION_MANAGER_H
 #define COLLISION_MANAGER_H
 #include "Exports.h"
+#include "Entity2D.h"
 
 class FLK_API CollisionManager
 {
 private:
 
 public:
+	bool CheckCollision(Entity2D &one, Entity2D &two);
 };
 
 #endif
